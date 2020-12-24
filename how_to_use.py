3aae1d433bd0f7d00fdda69a72b30ac7ab1dad8f
vanviethieuanh <vanviethieuanh@gmail.com>
2020-12-24 22:33:51 +0700

    Fix ratio problem

---
+++ how_to_use.py
@@ -0,0 +1,8 @@
+import scr.RemoveBg as rm
+import scr.AddBg as add
+import cv2
+
+obj, mask = rm.RemoveBg().remove_background('https://i.ytimg.com/vi/m9_uc4ggQyk/maxresdefault.jpg',6)
+result = add.AddBg(obj,mask,'https://travelgear.vn/blog/wp-content/uploads/2019/08/dia-diem-du-lich-va-canh-dep-nhat-ban.jpg',1)
+
+cv2.imwrite('result.jpg',result)
